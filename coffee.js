595867f523c94384d50ad9c19075d1d0017256a2
kevinfiero <kjf46@cornell.edu>
2020-10-05 14:34:54 -0700

    Step 2: Product Data

---
+++ coffee.js
@@ -0,0 +1,90 @@
+export const coffee = [
+    {
+        id: 'french-roast-12oz',
+        name: 'French Roast',
+        image: 'french-roast.png',
+        description: '12oz French Roast',
+        category: 'Organic',
+        price: 16
+    },
+    {
+        id: 'hair-bender-12oz',
+        name: 'Hair Bender',
+        image: 'hair-bender.png',
+        description: '12oz Hair Bender',
+        category: 'Organic',
+        price: 15
+    },
+    {
+        id: 'holler-mountatin-12oz',
+        name: 'Holler Mountain',
+        image: 'holler-mountain.png',
+        description: '12oz Holler Mountain',
+        category: 'Organic',
+        price: 16
+    },
+    {
+        id: 'house-blend-12oz',
+        name: 'House Blend',
+        image: 'house-blend.png',
+        description: '12oz House Blend',
+        category: 'Organic',
+        price: 15
+    },
+    {
+        id: 'trapper-creek-12oz',
+        name: 'Trapper Creek',
+        image: 'trapper-creek.png',
+        description: '12oz Trapper Creek',
+        category: 'Organic',
+        price: 16
+    },
+    {
+        id: 'el-puente-12oz',
+        name: 'Honduras El Puente',
+        image: 'el-puente.png',
+        description: '12oz Honduras El Puente',
+        category: 'Single Origin Orangic',
+        price: 20
+    },
+    {
+        id: 'el-injerto-bourbon-12oz',
+        name: 'Guatemala El Injerto Bourbon',
+        image: 'el-injerto-bourbon.png',
+        description: '12oz Guatemala El Injerto Bourbon',
+        category: 'Single Origin Orangic',
+        price: 20
+    },
+    {
+        id: 'marvin-robles-12oz',
+        name: 'Costa Rica Marvin Robles',
+        image: 'marvin-robles.png',
+        description: '12oz Costa Rica Marvin Robles',
+        category: 'Single Origin Orangic',
+        price: 22
+    },
+    {
+        id: 'bies-penantan-12oz',
+        name: 'Indonesia Bies Penatan',
+        image: 'bies-penantan.png',
+        description: '12oz Indonesia Bies Penatan',
+        category: 'Single Origin Orangic',
+        price: 19
+    },
+    {
+        id: 'ndaroini-12oz',
+        name: 'Kenya Ndaroini',
+        image: 'ndaroini.png',
+        description: '12oz Kenya Ndaroini',
+        category: 'Single Origin Orangic',
+        price: 19
+    },
+    {
+        id: 'mordecofe-12oz',
+        name: 'Ethiopia Mordecofe',
+        image: 'mordecofe.png',
+        description: '12oz Ethiopia Mordecofe',
+        category: 'Single Origin Orangic',
+        price: 20
+    }
+];
