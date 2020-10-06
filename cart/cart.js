c558632b28adedb65db6b4af90ba6b5c33d6fb2e
kevinfiero <kjf46@cornell.edu>
2020-10-06 15:51:06 -0700

    Lab07 - Step 5: TDD DOM Render function

---
+++ cart/cart.js
@@ -0,0 +1,10 @@
+// import { coffees } from '../data/coffee.js';
+// import { renderCoffee } from './product-utils.js';
+
+// const ul = document.querySelector('#productList');
+
+// for (let i = 0; i < coffees.length; i++){
+//     const coffee = coffees[i];
+//     const li = renderCoffee(coffee);
+//     ul.appendChild(li);
+// }
--- test/coffee.test.js
+++ test/coffee.test.js
@@ -1,30 +1,12 @@
 // IMPORT MODULES under test here:
 import { renderCoffee } from '../products/product-utils.js';
-import { findByID, calcLineItem } from '../cart/cart-utils.js';
+import { findByID, calcLineItem, renderLineItem } from '../cart/cart-utils.js';
 
 const test = QUnit.test;
 
-test('test rendering French Roast coffee', (expect) => {
-    const coffeeTest =
-    {
-        id: 'french-roast-12oz',
-        name: 'French Roast',
-        image: 'french-roast.png',
-        description: '12oz French Roast',
-        category: 'Organic',
-        price: 16
-    };
+//Create static test data
 
-    const actual = renderCoffee(coffeeTest).outerHTML;
-
-    const expected = '<li class="product"><h2>French Roast</h2><img src="../assets/french-roast.png"><h3>12oz French Roast</h3><h3>$16.00</h3><button id="french-roast-12oz">Add to Cart</button></li>';
-
-    expect.equal(actual, expected);
-});
-
-test('test finding object by ID', (expect) => {
-
-    const frenchRoast = 
+const frenchRoast = 
         {
             id: 'french-roast-12oz',
             name: 'French Roast',
@@ -34,7 +16,7 @@ test('test finding object by ID', (expect) => {
             price: 16
         };
 
-    const hairBender =
+const hairBender =
         {
             id: 'hair-bender-12oz',
             name: 'Hair Bender',
@@ -44,7 +26,7 @@ test('test finding object by ID', (expect) => {
             price: 15
         };
 
-    const hollerMountain =
+const hollerMountain =
     {
         id: 'holler-mountatin-12oz',
         name: 'Holler Mountain',
@@ -53,7 +35,26 @@ test('test finding object by ID', (expect) => {
         category: 'Organic',
         price: 16
     };
+
+const cartLineItem =    
+    {
+        id: 'french-roast-12oz',
+        quantity: 3
+    };
     
+
+test('test rendering French Roast coffee', (expect) => {
+
+
+    const actual = renderCoffee(frenchRoast).outerHTML;
+
+    const expected = '<li class="product"><h2>French Roast</h2><img src="../assets/french-roast.png"><h3>12oz French Roast</h3><h3>$16.00</h3><button id="french-roast-12oz">Add to Cart</button></li>';
+
+    expect.equal(actual, expected);
+});
+
+test('test finding object by ID', (expect) => {
+
     const coffeeArray = [frenchRoast, hairBender, hollerMountain];
 
     const id = 'hair-bender-12oz';
@@ -68,7 +69,7 @@ test('test finding object by ID', (expect) => {
     expect.equal(actual2, expected2);
 });
 
-test('test finding object by ID', (expect) => {
+test('test multiply quantity and amount', (expect) => {
 
     const quantity1 = 5;
     const amount1 = 10;
@@ -83,4 +84,13 @@ test('test finding object by ID', (expect) => {
 
     expect.equal(actual1, expected1);
     expect.equal(actual2, expected2);
+});
+
+test('test DOM render', (expect) => {
+
+    const actual = renderLineItem(cartLineItem, frenchRoast).outerHTML;
+    const expected = '<tr><td>French Roast</td><td>$16</td><td>3</td><td>$48</td></tr>';
+
+    expect.equal(actual, expected);
+
 });
