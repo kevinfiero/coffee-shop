afd4f90443a0cbe1d206910d01152681424106f3
kevinfiero <kjf46@cornell.edu>
2020-10-07 17:13:52 -0700

    Lab 8 - Successfully hid table and button before cart items added

--- cart/cart.js
+++ cart/cart.js
@@ -1,34 +1,21 @@
 // Import data and functions
 import { coffees } from '../data/coffee.js';
-import { renderLineItem, calcOrderTotal, togglePlaceOrderButton } from './cart-utils.js';
+import { renderLineItem, calcOrderTotal, hideCartElements } from './cart-utils.js';
 import { getFromLocalStorage, findByID } from '../utils.js';
 
-// Display the cart table on the page by looping through cart array to create line elements then adding total line
-
-const tbody = document.querySelector('#cartTable');
+const tbody = document.querySelector('#cartTable'); 
+const table = document.getElementsByTagName('table')[0]; 
+const messageSection = document.getElementsByTagName('section')[0]; 
 const cartString = 'cart';
 const localStorageCart = getFromLocalStorage(cartString) || [];
 const placeOrderButton = document.getElementById('place-order');
-
-
-// const tr = document.createElement('tr');
-// tr.innerHTML = '<tr><th>Product</th><th>Unit Cost</th><th>Quantity</th><th>Product Cost</th></tr>';
-// tbody.append(tr);
-
-
-// for (let i = 0; i < localStorageCart.length; i++){
-//     const cartItem = localStorageCart[i];
-//     const product = findByID(coffees, cartItem.id);
-//     const td = renderLineItem(cartItem, product);
-//     tbody.appendChild(td);
-// }
 
 if (localStorageCart.length > 0){
 
     const tr = document.createElement('tr');
     tr.innerHTML = '<tr><th>Product</th><th>Unit Cost</th><th>Quantity</th><th>Product Cost</th></tr>';
-    tbody.append(tr);   
-
+    tbody.append(tr);  
+    
     for (let i = 0; i < localStorageCart.length; i++){
         const cartItem = localStorageCart[i];
         const product = findByID(coffees, cartItem.id);
@@ -36,7 +23,7 @@ if (localStorageCart.length > 0){
         tbody.appendChild(td);
     }
 
-    togglePlaceOrderButton(placeOrderButton, false);
+    hideCartElements(placeOrderButton, table, messageSection, false);
     const totalLineTD = document.createElement('tr');
     const total = calcOrderTotal(localStorageCart, coffees);
     totalLineTD.innerHTML = `<tr><td colspan="3">Total</td><td>$${total}</td></tr>`;
@@ -44,7 +31,7 @@ if (localStorageCart.length > 0){
 
 } else {
 
-    togglePlaceOrderButton(placeOrderButton, true);
+    hideCartElements(placeOrderButton, table, messageSection, true);
 }
 
 placeOrderButton.addEventListener('click', () => {
@@ -52,8 +39,7 @@ placeOrderButton.addEventListener('click', () => {
     const alertString = JSON.stringify(localStorageCart, true, 2);
     alert(alertString);
     window.location.replace('../index.html');
-
     localStorage.removeItem(cartString);
-    togglePlaceOrderButton(placeOrderButton, true);
+    hideCartElements(placeOrderButton, table, messageSection, true);
 
 });
